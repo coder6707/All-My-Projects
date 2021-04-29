710c77cd209479d65ca59b18db0342e814d20170
coder6707 <71891557+coder6707@users.noreply.github.com>
2021-04-29 13:44:09 +0530

    Update Website_URL_Checker.py
    So you basically enter a website and it tells you if the url was correct, incorrect, was redirected , etc...

--- Website_URL_Checker.py
+++ Website_URL_Checker.py
@@ -2,5 +2,5 @@ import requests
 url = input("Enter url: ")
 url = "https://" + url
 response = requests.get(url)
-
+print("Informational responses (100–199) \n Successful responses (200–299) \n Redirects (300–399) \n Client errors (400–499) \n Server errors (500–599)")
 print(f"your request to {url}  came back w/ status code {response.status_code}")
